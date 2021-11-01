3c5fcdae42f72f27b5bbd0d65ef7a06f7b2bb0e5
santhoshkumar9913 <44202675+santhoshkumar9913@users.noreply.github.com>
2021-11-01 23:02:45 +0530

    Update hack.js

--- src/hack.js
+++ src/hack.js
@@ -9,7 +9,8 @@ const hash = '5e7d28e2cfff93edefb2d15abad07ec5';
 // When you figure it out, create a Pull Request on github with value. 
 // First correct PR wins a Lifetime PRO  membership and T-shirt
 
-///// ANSWER /////
+///// ANSWER ///// 
 
 const hacked = 'superhacker';
 
+//i cracked it without knowing the answer
